0fcfdc1a1ee2c65b1ed6eeff7c7f38fc4ccab236
Nicole Silva - Padrao <nicolesilva8144@gmail.com>
2023-01-05 12:38:58 -0300

    Estruturas de Classes

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse{
+
+    public static void main(String[] args){
+
+
+        System.out.println("Nessa aula aprendemos as estruturas básicas de uma classe em Java");
+    }
+}
