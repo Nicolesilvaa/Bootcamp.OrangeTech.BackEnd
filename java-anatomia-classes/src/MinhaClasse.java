e81c9f94b28cdf0b1aa316d8d2eb5a4635ebac30
Nicole Silva - Padrao <nicolesilva8144@gmail.com>
2023-01-05 19:30:19 -0300

    Brincando com classes

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -4,5 +4,17 @@ public class MinhaClasse{
 
 
         System.out.println("Nessa aula aprendemos as estruturas básicas de uma classe em Java");
+
+        
+        String primeiroNome = "Nicole";
+        String segundoNome = "Silva";
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
---
+++ java-anatomia-classes/src/ParImpar.java
@@ -0,0 +1,11 @@
+public class ParImpar{
+
+    public static void main( String []args){
+
+        int numero1 = 5, int numero2 = 8
+        int somar(numero1, numero2){
+
+            
+        }
+    }
+}
