3f2d500eb092662edaf3fe7781deef0b29fdb81c
Nicole Silva - Padrao <nicolesilva8144@gmail.com>
2023-01-05 20:58:25 -0300

    Identando classes

--- java-anatomia-classes/src/edu/nicole/sintaxe/parImpar.java
+++ java-anatomia-classes/src/edu/nicole/sintaxe/parImpar.java
@@ -3,7 +3,6 @@ package edu.nicole.sintaxe;
 public class parImpar{
 
     public static boolean parImpar(int numero){
-
         return(numero % 2 == 0);
         
     }
@@ -11,15 +10,12 @@ public class parImpar{
     public static void main( String []args){
 
         int numero = 5;
-
         boolean resultado = parImpar(numero);
 
         if(parImpar(numero) == true)
-
             System.out.println("O número é par");
 
         else
-
             System.out.println("O número é ímpar");
         
     }
