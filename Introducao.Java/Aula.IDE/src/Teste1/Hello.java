381c50b3fc52c732b3b3cd5884db421d5c57630e
Nicole Silva - Padrao <nicolesilva8144@gmail.com>
2023-01-04 21:59:30 -0300

    Novo projeto Java

--- Introducao.Java/Aula.IDE/src/Teste1/Hello.java
+++ Introducao.Java/Aula.IDE/src/Teste1/Hello.java
@@ -4,8 +4,12 @@ public class Hello {
 
 	public static void main(String[]args) {
 		
-		System.out.println("Olá mundo");
+		System.out.println("Olá mundo\n");
+		System.out.println(".....................................\n");
+		System.out.println("Esse é o meu primeiro código em Java!");
+		System.out.println(".....................................\n");
 		
 	}
 	
 }
+;
